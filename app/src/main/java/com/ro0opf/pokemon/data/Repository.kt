98ddc2832dff94ef8c2fc5551ada5704f604d5ce
ro0opf <34717634+ro0opf@users.noreply.github.com>
2020-11-20 12:47:40 +0900

    handler to coroutine

--- app/src/main/java/com/ro0opf/pokemon/data/Repository.kt
+++ app/src/main/java/com/ro0opf/pokemon/data/Repository.kt
@@ -3,6 +3,8 @@ package com.ro0opf.pokemon.data
 import com.ro0opf.pokemon.common.RetrofitService
 import com.ro0opf.pokemon.data.pokemon.PokemonAPI
 import com.ro0opf.pokemon.data.pokemon.PokemonOfficialAPI
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object Repository{
     private val pokemonClient = RetrofitService.createService(PokemonAPI::class.java)
@@ -11,5 +13,7 @@ object Repository{
     suspend fun fetchPokemonList() = pokemonClient.fetchPokemonList()
     suspend fun fetchPokemonLocationList() = pokemonClient.fetchPokemonLocationList()
 
-    suspend fun fetchPokemonDetail(id : Int) = pokemonOfficialClient.fetchPokemonDetail(id)
+    suspend fun fetchPokemonDetail(id : Int) = withContext(Dispatchers.IO){
+        pokemonOfficialClient.fetchPokemonDetail(id)
+    }
 }
--- app/src/main/java/com/ro0opf/pokemon/ui/SplashActivity.kt
+++ app/src/main/java/com/ro0opf/pokemon/ui/SplashActivity.kt
@@ -2,17 +2,20 @@ package com.ro0opf.pokemon.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ro0opf.pokemon.R
 import com.ro0opf.pokemon.databinding.ActivitySplashBinding
 import com.ro0opf.pokemon.ui.search.SearchActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT: Long = 1500
     private lateinit var binding: ActivitySplashBinding
+    private val activityScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +25,11 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun startLoginActivity() {
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, SearchActivity::class.java))
+        activityScope.launch {
+            delay(SPLASH_TIME_OUT)
+            startActivity(Intent(this@SplashActivity, SearchActivity::class.java))
             overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
             finish()
-        }, SPLASH_TIME_OUT)
+        }
     }
 }
