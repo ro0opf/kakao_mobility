fbdf6142ddfe3b023d71a95b0555dd93b2d1e6d3
mike(조재혁) <mike.j@kakaomobility.com>
2021-02-04 16:20:52 +0900

    Update MapsActivity.kt

--- app/src/main/java/com/ro0opf/pokemon/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/ro0opf/pokemon/ui/maps/MapsActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.ro0opf.pokemon.R
+import com.ro0opf.pokemon.data.pokemon.PokemonLocationList
 import com.ro0opf.pokemon.ui.pokemondetail.PokemonDetailViewModel
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -19,26 +20,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-//        val mapFragment = supportFragmentManager
-//            .findFragmentById(R.id.map) as SupportMapFragment
-//        mapFragment.getMapAsync(this)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-//        mMap = googleMap
-//        val pokemonLocationList =
-//            intent.getParcelableExtra<PokemonDetailViewModel.PokemonLocationList>("pokemonLocationList")!!
-//
-//        val locationList = pokemonLocationList.locations!!
-//        val firstZoom = LatLng(locationList[0].lat, locationList[0].lng)
-//
-//        for (location in locationList) {
-//            mMap.addMarker(
-//                MarkerOptions().position(LatLng(location.lat, location.lng))
-//                    .title(pokemonLocationList.names[0])
-//            )
-//        }
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(firstZoom))
-//        mMap.animateCamera(CameraUpdateFactory.zoomTo(10.0f))
+        mMap = googleMap
+        val pokemonLocationList =
+            intent.getParcelableExtra<PokemonLocationList>("pokemonLocationList")!!
+
+        val locationList = pokemonLocationList.locations
+        val firstZoom = LatLng(locationList[0].lat, locationList[0].lng)
+
+        for (location in locationList) {
+            mMap.addMarker(
+                MarkerOptions().position(LatLng(location.lat, location.lng))
+                    .title(pokemonLocationList.names[0])
+            )
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(firstZoom))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10.0f))
     }
 }
